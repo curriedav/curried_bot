334b4849083ee04f65864c2818cb66dfa3656ac7
curriedav <curriedav@gmail.com>
2013-12-12 10:24:05 -0800

    adds a shout function

--- bot.js
+++ bot.js
@@ -4,9 +4,21 @@ var client = new irc.Client('irc.freenode.net', 'curried_bot', {
 	channels: ['#pdxbots']
 });
 
+// client.addListener('message', function(from, to, message) {
+// 	console.log(from + 'said' + message + 'to' + to);
+// 	if (/^ping$/.test(message)) {
+// 		client.say(to, 'pong');
+// 	};
+// });
+
 client.addListener('message', function(from, to, message) {
 	console.log(from + 'said' + message + 'to' + to);
-	if (/^ping$/.test(message)) {
-		client.say(to, 'pong');
+	
+	var result = /^!shout (.*)$/.exec(message);
+
+
+
+	if (result) {
+		client.say(to, result[1].toUpperCase());
 	};
 });
